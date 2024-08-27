a2d02fa679e259ca8f6aa5b84f00e4cf5a324c17
NSStoyanova22 <NSStoyanova22@codingburgas.bg>
2024-08-27 04:45:34 +0300

    Make changes in the navbar ui

--- app/page.tsx
+++ app/page.tsx
@@ -17,17 +17,30 @@ export default function Home() {
       <NavigationMenu className=" p-4">
         <NavigationMenuList>
           <NavigationMenuItem className="px-3">
-          <div className="bg-black w-9 h-[0.4rem] rounded mb-[0.2rem]"></div>
-          <div className="bg-black w-9 h-[0.4rem] rounded mb-[0.2rem]"></div>
-          <div className="bg-black w-9 h-[0.4rem] rounded mb-[0.2rem]"></div>
+          <div className="bg-slate-400 w-[4rem] h-[0.46rem] rounded mb-[0.27rem]"></div>
+          <div className="bg-slate-400 w-[4rem] h-[0.46rem] rounded mb-[0.27rem]"></div>
+          <div className="bg-slate-400 w-[4rem] h-[0.46rem] rounded mb-[0.27rem]"></div>
           
           </NavigationMenuItem>
           <NavigationMenuItem>
             <Link href="#" legacyBehavior passHref>
-              <NavigationMenuLink className="font-bold text-5xl">
+              <NavigationMenuLink className="font-bold text-slate-400 text-5xl ml-3">
                 EventHub
               </NavigationMenuLink>
             </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+          <input 
+            type="text" 
+            placeholder="Search..."
+            className="w-[45rem] ml-[7rem] px-4 py-2 border-[0.15rem] border-gray-300 rounded-3xl focus:outline-none focus:ring-0 focus:border-gray-300 shadow"
+          />
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+          <div className="relative inline-block ml-4 w-10 h-10 mt-2">
+      <div className="absolute top-0 left-0 w-[2rem] h-[2rem] border-[0.4rem] border-[#2E4F6F] rounded-full"></div>
+      <div className="absolute mr-1 mb-[-0.1.5rem] bottom-0 right-0 w-5 h-2 bg-[#2E4F6F] transform rotate-45 origin-bottom-right"></div>
+    </div>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
